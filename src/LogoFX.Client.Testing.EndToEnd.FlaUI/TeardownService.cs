7617c6cfa45740f285f61e0240150588a8aa23ca
Gennady Verdel <godrose@gmail.com>
2020-04-17 10:46:58 +0300

    init: added initial facilities

---
+++ src/LogoFX.Client.Testing.EndToEnd.FlaUI/TeardownService.cs
@@ -0,0 +1,14 @@
+﻿using Attest.Testing.Contracts;
+
+namespace LogoFX.Client.Testing.EndToEnd.FlaUI
+{
+    /// <inheritdoc />
+    public sealed class TeardownService : ITeardownService
+    {
+        /// <inheritdoc />
+        public void Teardown()
+        {
+            ApplicationContext.Application?.Kill();
+        }
+    }
+}
